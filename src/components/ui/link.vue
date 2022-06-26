b3cc2b7d4000a3fa4facf64b8736c403b30f85c0
Cade Embery <s-cade@subsplash.com>
2022-06-26 15:58:03 +1000

    added spa navigation for ux-link

--- src/components/ui/link.vue
+++ src/components/ui/link.vue
@@ -26,7 +26,7 @@ export default {
             if(self.buildMode || self.nuxtMode) {
                 event.preventDefault();
                 event.stopImmediatePropagation();
-                console.log('hijack spa');
+                console.log('hijack spa', self.$qik.app);
                 return self.$qik.app.to(self.to);
             }
         },
