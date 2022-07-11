06700540fe0d14ad046f83af923838633bd82210
Cade Embery <s-cade@subsplash.com>
2022-07-11 11:34:46 +1000

    removed ace extra to reduce window dependency

--- src/components/form/inputs/code-editor.vue
+++ src/components/form/inputs/code-editor.vue
@@ -6,56 +6,13 @@
 </template>
 <script>
 
-import ace from 'ace-builds';
-import { version } from 'ace-builds';
-ace.config.set("basePath", `https://cdn.jsdelivr.net/npm/ace-builds@${version}/src-noconflict/`)
-
-import { VAceEditor } from 'vue3-ace-editor';
-
-export default {
-    components: {
-        VAceEditor,
-    },
-    methods: {
-        editorInit() {
-
-        }
-    },
-    props: {
-        modelValue: {
-            type: String,
-            default () {
-                return '';
-            }
-        },
-        lang:{
-            type:String,
-        },
-    },
-    watch:{
-        modelValue(v) {
-            this.model = v;
-        },
-        model(v) {
-            this.$emit('update:modelValue', v);
-        }
-    },
-    mounted() {
-        this.mounted = true;
-    },
-    data() {
-        return {
-            mounted:false,
-            model: this.modelValue,
-        }
-    },
-}
 /**
 import ace from 'ace-builds';
 import { version } from 'ace-builds';
 ace.config.set("basePath", `https://cdn.jsdelivr.net/npm/ace-builds@${version}/src-noconflict/`)
-import { VAceEditor } from 'vue3-ace-editor';
+/**/
 
+import { VAceEditor } from 'vue3-ace-editor';
 
 export default {
     components: {
@@ -95,8 +52,6 @@ export default {
         }
     },
 }
-
-/**/
 </script>
 <style lang="scss" scoped>
 .editor-wrap {
